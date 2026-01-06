72f906d00e34c7dccd22097c42b6b264d6862c2f
Tschoppsilas <silas.tschopp@breitband.ch>
2026-01-06 15:19:11 +0100

    Versuch_2

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,11 +1,10 @@
-import pytest
-import pandas as pd
 import sys
 from pathlib import Path
 
 # Sicherstellen, dass der "Python"-Ordner im Systempfad ist
 root_dir = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(root_dir / "Python"))
+
 
 def test_setup():
     # Dieser Test stellt sicher, dass die Pipeline technisch funktioniert
